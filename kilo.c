07cd27c275aab41a31cf80cc909e3403ac179bdb
raviwhitehat <ekravi26@gmail.com>
2025-12-04 21:52:08 +0530

    Refactor raw mode handling and improve syntax highlighting for decimal numbers

--- kilo.c
+++ kilo.c
@@ -12,28 +12,58 @@
 DWORD orig_mode;
 HANDLE global_hIn;
 
+static HANDLE g_hStdin = NULL;
+static HANDLE g_hStdout = NULL;
+static DWORD g_origInMode = 0;
+static DWORD g_origOutMode = 0;
+
+void die(const char*);
+void disableRawMode();
+void enableRawMode();
+int editorReadKey();
+
+
+
 void die(const char *s){
     perror(s);
     exit(EXIT_FAILURE);
 }
 
-
 void disableRawMode(){
-if(SetConsoleMode(global_hIn,orig_mode) == -1) die("Error on Disable Raw Mode Set Console Mode");
+    if (g_hStdin && g_origInMode) SetConsoleMode(g_hStdin, g_origInMode);
+    if (g_hStdout && g_origOutMode) SetConsoleMode(g_hStdout, g_origOutMode);
 }
 
 void enableRawMode() {
-    global_hIn = GetStdHandle(STD_INPUT_HANDLE);
-    DWORD mode = orig_mode;
-    if(GetConsoleMode(global_hIn, &mode) == -1) die('Error On Enable Raw Mode Get Console Mode ');
-    atexit(disableRawMode);
-    
+    g_hStdin = GetStdHandle(STD_INPUT_HANDLE);
+    g_hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
 
+    if(g_hStdin == INVALID_HANDLE_VALUE || g_hStdout == INVALID_HANDLE_VALUE) die("GetStdHandle");
+    if(!GetConsoleMode(g_hStdin, &g_origInMode)) die("GetConsoleGStdIn");
+    if(!GetConsoleMode(g_hStdout, &g_origOutMode)) die("GetConsoleGStdOut");
+
+    #ifndef ENABLE_QUICK_EDIT_MODE
+    #define ENABLE_QUICK_EDIT_MODE 0x0040
+    #endif
+    #ifndef ENABLE_VIRTUAL_TERMINAL_PROCESSING
+    #define ENABLE_VIRTUAL_TERMINAL_PROCESSING 0x0004
+    #endif
+    
+    DWORD inMode = g_origInMode;
+    inMode &= ~(ENABLE_ECHO_INPUT | ENABLE_LINE_INPUT | ENABLE_PROCESSED_INPUT | ENABLE_MOUSE_INPUT | ENABLE_QUICK_EDIT_MODE);
+    if (!SetConsoleMode(g_hStdin, inMode)) die("SetConsoleMode input");
+    //global_hIn = GetStdHandle(STD_INPUT_HANDLE);
+    // DWORD mode = orig_mode;
+    DWORD outMode = g_origOutMode;
+    outMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    // if(GetConsoleMode(global_hIn, &mode) == -1) die('Error On Enable Raw Mode Get Console Mode ');
+    // atexit(disableRawMode);
+    SetConsoleMode(g_hStdout, outMode);
+    atexit(disableRawMode);
     //To Turn Off ECHO and Special Inputs
     // mode &= ~(ENAB )
     
-    mode &= ~(ENABLE_ECHO_INPUT | ENABLE_LINE_INPUT | ENABLE_PROCESSED_INPUT );
-    if( SetConsoleMode(global_hIn, mode) == -1) die("Error on Enabling Raw Mode Set Console Mode");
+    // if( SetConsoleMode(global_hIn, mode) == -1) die("Error on Enabling Raw Mode Set Console Mode");
 }
 
 
--- kiloLinux.c
+++ kiloLinux.c
@@ -30,7 +30,6 @@ char* render;
 unsigned char *hl;
 } erow;
 
-
 struct editorConfig{
 int cx,cy;
 int rx;
@@ -136,8 +135,8 @@ void editorUpdateSyntax(erow* row){
 	int i = 0;
 	while(i < row->rsize) {
 		char c = row->render[i];
-		unsigned char prev_hl = (i > 0} ? row->hl[i-1] : HL_NORMAL;
-		if (isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)){
+		unsigned char prev_hl = (i > 0) ? row->hl[i-1] : HL_NORMAL;
+		if ((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) || (c == '.' && prev_hl == HL_NUMBER)){
 			row->hl[i] = HL_NUMBER;
 			prev_sep = 0;
 			i++;
